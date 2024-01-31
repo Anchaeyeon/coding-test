ac0f103706fc0baaac86e985029411773839a7e3
Anchaeyeon <142782110+Anchaeyeon@users.noreply.github.com>
2024-02-01 00:52:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int N, X;
+    int A[10000];
+    
+    scanf("%d%d", &N, &X);
+    
+    for (int i=0; i<N; i++) {
+        scanf("%d", &A[i]);
+        if(A[i] < X) printf("%d ", A[i]);
+    }
+}
